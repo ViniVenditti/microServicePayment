2e914dfb051e142af58a4d8e6235552285c4a3d3
a2w <a2w@localhost.localdomain>
2020-08-13 11:35:23 -0300

    chore: inclusão de tratamento de erro

--- src/main/java/br/com/vinicius/pagamentos/payments/controller/PaymentController.java
+++ src/main/java/br/com/vinicius/pagamentos/payments/controller/PaymentController.java
@@ -1,12 +1,14 @@
 package br.com.vinicius.pagamentos.payments.controller;
 
 import br.com.vinicius.pagamentos.payments.entity.PaymentEntity;
+import br.com.vinicius.pagamentos.payments.exceptions.CardNotFoundException;
 import br.com.vinicius.pagamentos.payments.exceptions.CardNotHabilitException;
 import br.com.vinicius.pagamentos.payments.mapper.PaymentMapper;
 import br.com.vinicius.pagamentos.payments.model.CardClient;
 import br.com.vinicius.pagamentos.payments.model.CardModel;
 import br.com.vinicius.pagamentos.payments.model.PaymentModel;
 import br.com.vinicius.pagamentos.payments.service.PaymentService;
+import feign.FeignException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,12 +29,16 @@ public class PaymentController {
 
     @PostMapping
     public ResponseEntity<PaymentModel> doPayment(@RequestBody PaymentModel payment) {
-        if (cardClient.verifyCard(payment.getCardId())){
-            PaymentEntity entity = mapper.from(payment);
-            PaymentModel model = service.savePayment(entity);
-            return ResponseEntity.status(HttpStatus.CREATED).body(model);
-        } else
-            throw new CardNotHabilitException();
+        try{
+            if (cardClient.verifyCard(payment.getCardId())){
+                PaymentEntity entity = mapper.from(payment);
+                PaymentModel model = service.savePayment(entity);
+                return ResponseEntity.status(HttpStatus.CREATED).body(model);
+            } else
+                throw new CardNotHabilitException();
+        } catch (FeignException.NotFound e) {
+            throw new CardNotFoundException();
+        }
 
     }
 
